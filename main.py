98ff15b0b46cc0206da02aabe634377a608d54bb
jordansbenjamin <14327@coderacademy.edu.au>
2023-06-13 11:17:32 +1000

    Created player class and turtle object

--- main.py
+++ main.py
@@ -8,7 +8,10 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
+turtle = Player()
+
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+    screen.exitonclick()
